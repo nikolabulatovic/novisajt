7815c060a57949edb8f3a5031bc0de857b90f48c
nikolabulatovic <bulatovic_nikola@yahoo.com>
2025-12-19 10:25:38 +0100

    remove the purple circular background from CharacterEvaluation

--- components/CharacterEvaluation.tsx
+++ components/CharacterEvaluation.tsx
@@ -88,8 +88,8 @@ export default function CharacterEvaluation({
 
       {/* Background */}
       <div className='absolute inset-0 overflow-hidden'>
-        <div className='absolute top-1/4 left-1/4 w-96 h-96 bg-purple-500/5 rounded-full blur-3xl animate-pulse' />
-        <div className='absolute bottom-1/4 right-1/4 w-96 h-96 bg-indigo-500/5 rounded-full blur-3xl animate-pulse delay-1000' />
+        <div className='absolute top-1/4 left-1/4 w-96 h-96 rounded-full blur-3xl animate-pulse' />
+        <div className='absolute bottom-1/4 right-1/4 w-96 h-96 rounded-full blur-3xl animate-pulse delay-1000' />
       </div>
 
       <div className='relative z-10 max-w-3xl mx-auto w-full'>
